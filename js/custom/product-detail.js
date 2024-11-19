cf67b14d4c3be0d45109406ca93134dd13b0056b
shaheem-pp <shanofficial2000@gmail.com>
2024-11-18 19:54:13 -0500

    product details update

--- js/custom/product-detail.js
+++ js/custom/product-detail.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+	const allSizes = ["XS", "S", "M", "L", "XL", "XXL"];
+	let productQuantity = 0;
+	let maxQuantity = 0;
+
 	function getProductIdFromUrl() {
 		const urlParams = new URLSearchParams(window.location.search);
 		return urlParams.get('id');
@@ -23,8 +27,49 @@ $(document).ready(function () {
 			$("#product-price").text(product.price);
 			$("#product-category").text(`Category: ${product.category}`);
 			$("#product-description").html(formatDescription(product.description));
-			$("#product-stock").text(`In Stock: ${product.quantity}`);
 			$("#product-image").attr("src", product.image);
+			productQuantity = product.quantity;
+			maxQuantity = productQuantity;
+
+			$("#inStock").text(`In Stock: ${maxQuantity}`);
+
+			$('#available-colors').append(`
+				<div class="d-flex align-items-center m-1">
+					<div style="width: 20px; height: 20px; background-color: ${product.colorCode};" class="rounded"></div>
+					<span class="ms-2">${product.color}</span>
+				</div>
+				`);
+
+
+
+			let selectedSize = '';
+			$('#available-sizes').empty();
+			allSizes.forEach(size => {
+				const isSizeAvailable = product.size.includes(size);
+				const sizeClass = isSizeAvailable ? 'btn-outline-dark' : 'btn-outline-secondary';
+				const sizeTitle = isSizeAvailable ? size : `${size}`;
+				$('#available-sizes').append(`
+        			<button type="button" class="btn ${sizeClass} m-1" ${isSizeAvailable ? '' : 'disabled'}>${sizeTitle}</button>
+    			`);
+			});
+			$('#available-sizes').on('click', 'button', function () {
+				if ($(this).hasClass('btn-outline-dark')) {
+					selectedSize = $(this).text();
+					$('#available-sizes button').each(function () {
+						if ($(this).text() === selectedSize) {
+							$(this).removeClass('btn-outline-dark').addClass('btn-dark');
+						} else if ($(this).hasClass('btn-dark')) {
+							$(this).removeClass('btn-dark').addClass('btn-outline-dark');
+						}
+					});
+					console.log(selectedSize);
+					
+				}
+			});
+
+
+
+
 		} else {
 			window.location.href = "index.html";
 		}
@@ -32,4 +77,29 @@ $(document).ready(function () {
 		console.error("Failed to load product data.");
 		window.location.href = "index.html";
 	});
+
+	$('#decrement').click(function () {
+		let currentQuantity = parseInt($('#quantity').val());
+		if (currentQuantity > 0) {
+			$('#quantity').val(currentQuantity - 1);
+		} else {
+			flashBorder();
+		}
+	});
+
+	$('#increment').click(function () {
+		let currentQuantity = parseInt($('#quantity').val());
+		if (currentQuantity < maxQuantity) {
+			$('#quantity').val(currentQuantity + 1);
+		} else {
+			flashBorder();
+		}
+	});
+
+	function flashBorder() {
+		$('#quantity').addClass('flash-border');
+		setTimeout(function () {
+			$('#quantity').removeClass('flash-border');
+		}, 500);
+	}
 });
--- js/script.js
+++ js/script.js
@@ -15,15 +15,12 @@ function ifUserInSession(goto) {
 function greetUser() {
 	const userId = sessionStorage.getItem('userId');
 	const usersData = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : null;
-	if (usersData && userId) {
-		// Find the user with the matching userId
-		const user = usersData.find(user => user.id == userId);
 
-		// Get the current hour
+	if (usersData && userId) {
+		const user = usersData.find(user => user.id == userId);
 		const currentHour = new Date().getHours();
 		let greeting;
 
-		// Determine the greeting based on the time of day
 		if (currentHour < 12) {
 			greeting = "Good Morning";
 		} else if (currentHour < 18) {
@@ -32,7 +29,6 @@ function greetUser() {
 			greeting = "Good Evening";
 		}
 
-		// If user is found, update the greeting text
 		if (user) {
 			$('#greetings').text(`${greeting}, ${user.name}`);
 		} else {
@@ -43,7 +39,6 @@ function greetUser() {
 	}
 }
 
-// Call the function
 $(document).ready(function () {
 	greetUser();
 });
